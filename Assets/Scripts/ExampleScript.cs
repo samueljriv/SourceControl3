e21668ceb2aca2bd0627669134f0c79c53b91288
samueljriv <samueljoelrivera5@outlook.com>
2026-05-20 13:28:26 -0400

    Test

--- Assets/Scripts/ExampleScript.cs
+++ Assets/Scripts/ExampleScript.cs
@@ -5,7 +5,8 @@ public class ExampleScript : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        Debug.Log("Hello world!")
+        Debug.Log("Hello world!");
+        Debug.Log("Test");
     }
 
     // Update is called once per frame
